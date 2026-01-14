15484faa689e59c2eeeb9b5c1024393c98772878
Karan Jaswal <118061359+Jaswal-12@users.noreply.github.com>
2026-01-14 11:59:54 +0530

    Add script to check if 3rd last character is a vowel

---
+++ 01_Python_Basics/WAP to check if the 3rd last character of a string is a vowel or not.py
@@ -0,0 +1,8 @@
+s=str(input("Enter text:"));
+
+x=s[2]
+if (x == 'A' or x == 'E' or x == 'I' or x == 'O' or x == 'U' or
+    x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u'):
+    print("Yes it is a vowel")
+else:
+    print("No it is not a vowel")
